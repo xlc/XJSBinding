f177b9f2d295f8f22183164a945852ad25ef8404
Xiliang Chen <xlchen1291@gmail.com>
2013-09-06 21:21:25 +1200

    Initial Commit

---
+++ XJSBinding/XJSBinding.h
@@ -0,0 +1,13 @@
+//
+//  XJSBinding.h
+//  XJSBinding
+//
+//  Created by Xiliang Chen on 13-9-6.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XJSBinding : NSObject
+
+@end
---
+++ XJSBinding/XJSBinding.m
@@ -0,0 +1,13 @@
+//
+//  XJSBinding.m
+//  XJSBinding
+//
+//  Created by Xiliang Chen on 13-9-6.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import "XJSBinding.h"
+
+@implementation XJSBinding
+
+@end
