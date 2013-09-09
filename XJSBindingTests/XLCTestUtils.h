7ad169476f60ac6b780e0c29305eb315cf8b79b0
Xiliang Chen <xlchen1291@gmail.com>
2013-09-09 23:26:33 +1200

    add XLCTestUtilsMemoryDebug to help ARC memory debug

--- XJSBindingTests/XLCTestUtils.h
+++ XJSBindingTests/XLCTestUtils.h
@@ -28,4 +28,35 @@ static inline void XLCRunloopRun(CFTimeInterval timeout)
     XLCRunloopRunOnce();
 }
 
+@interface NSObject (XLCTestUtilsMemoryDebug)
+
+/**
+ * Perform isa-swizzle to override retain / release / autorelease.
+ */
+- (id)xlc_swizzleRetainRelease;
+
+/**
+ * Restore isa.
+ */
+- (void)xlc_restoreRetainRelease;
+
+/**
+ * Count of autorelease called.
+ * This is can be useful to calculate "real" retain count given no
+ * autorelease pool was drain (which is the typical case when unit testing).
+ */
+- (NSUInteger)xlc_autoreleaseCount;
+
+// stub methods
+- (id)xlc_retain;
+- (void)xlc_release;
+- (id)xlc_autorelease;
+- (Class)xlc_class;
+- (void)xlc_dealloc;
+
+// not swizzled, just call original retainCount
+- (NSUInteger)xlc_retainCount;
+
+@end
+
 __END_DECLS
