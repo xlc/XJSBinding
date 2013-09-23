eca2db5becb157fde2654d34f5ddd35ad19aa6ea
Xiliang Chen <xlchen1291@gmail.com>
2013-09-23 23:11:17 +1200

    add target for on device tests

---
+++ XJSBindingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XJSBindingApp
+//
+//  Created by Xiliang Chen on 13-9-23.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
